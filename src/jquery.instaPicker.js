d299638a15f80d9ba3d8052b2d08eab6a7a356b8
Cody Byrnes <byrnes.cody@gmail.com>
2013-05-28 18:00:37 -0700

    Working on transfering back to dom from picker

--- src/jquery.instaPicker.js
+++ src/jquery.instaPicker.js
@@ -2,6 +2,7 @@ $.fn.instaPicker = function () {
     var imageHolder;
     var accessToken;
     var UID;
+    var selectedImages = {selected: []};
     function initializer(){
         accessToken = window.location.hash.split("=")[1];
         if (accessToken != undefined) {
@@ -50,16 +51,42 @@ $.fn.instaPicker = function () {
             "overflow"   : "scroll"
 
         }
-        $("body").append("<div id='lightbox'> <div id='lightbox-container'> <div id='instagram-photos'></div> <div id=''>SUBMIT</div> </div> </div>");
+        photoSelectedStyle = {
+            "opacity"    : ".6"
+        }
+        $("body").append("<div id='lightbox'> <div id='lightbox-container'> <div id='instagram-photos'></div> <div id='instaPick-submit'>SUBMIT</div> </div> </div>");
         $("#lightbox").css(lightboxStyle);
         $("#lightbox #lightbox-container").css(lightboxContainerStyle);
         $("#lightbox #lightbox-container #instagram-photos").css(photoContainerStyle);
-        $("#lightbox").fadeIn();
+        $("#lightbox #lightbox-container #instagram-photos").css("display", "block");
+
+        $("#lightbox").fadeIn(function(){
+            $(".instagram-picture").click(function(){
+                if ($(this).hasClass("instaPicked")){
+                    $(".instaPicked").css("opacity", "1");
+                    $(this).removeClass("instaPicked");
+
+                } else {
+                    $(this).addClass("instaPicked");
+                    $(".instaPicked").css(photoSelectedStyle);
+                }
+
+            });
+            $("#instaPick-submit").click(function(){
+                $(".instaPicked").each(function(index, value){
+                    selectedImages.selected.push({element: $(value), hiRes: $(value).data("highres") });
+                    console.log(selectedImages);
+                    closeLightBox();
+                })
+
+            });
+
+        });
 
         function display(photos) {
             console.log(photos);
             for (x = 0; x < photos.data.length; x++) {
-                $("#instagram-photos").append("<img class='' width='100' height='100' data-highRes=''" + photos.data[x].images.standard_resolution.url + "' src='" + photos.data[x].images.low_resolution.url + "' >")
+                $("#instagram-photos").append("<img class='instagram-picture' width='100' height='100' data-highRes='" + photos.data[x].images.standard_resolution.url + "' src='" + photos.data[x].images.low_resolution.url + "' >")
             }
         }
 
@@ -68,6 +95,9 @@ $.fn.instaPicker = function () {
     function closeLightBox(){
         $("#lightbox").fadeOut();
         $("#lightbox").remove();
+        for (var i = 0; i < selectedImages.selected.length; i++ ){
+            selectedImages.selected[i].element.clone().appendTo(imageHolder);
+        }
     }
 
 
